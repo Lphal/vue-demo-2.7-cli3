6dd60ddf1c2992d4b591e14695d26ddbc6996a6c
lipenghui <lphal@qq.com>
2022-07-26 09:19:45 +0800

    update to vue2.7

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <span>{{ count }}</span>
   </div>
 </template>
+<script lang="js">
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  created() {
+    debugger
+    this.count++
+    console.log('count:', this.count)
+  }
+}
+</script>
